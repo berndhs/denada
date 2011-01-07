7b3382684853fad808a9a11f962fef494dfd859f
Bernd Stramm <bernd.stramm@gmail.com>
2011-01-07 12:08:18 -0500

    change copyright year

--- src/gpl2.h
+++ src/gpl2.h
@@ -3,7 +3,7 @@
 /****************************************************************
  * This file is distributed under the following license:
  *
- * Copyright (C) 2010, Bernd Stramm
+ * Copyright (C) 2011, Bernd Stramm
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
